943a027fa2ab5c1ee4003131d988d41edee7e33f
Jiale Hou <81398307+houseu2000@users.noreply.github.com>
2024-06-24 13:37:33 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,410 @@
+# Author:侯佳乐
+# Time:2022/12/21 23:34
+# encoding=utf-8
+
+import scipy.io
+import scipy.linalg
+import sklearn.metrics
+from sklearn.svm import SVC
+import numpy as np
+import scipy.linalg
+import sklearn.metrics
+import sklearn.neighbors
+from sklearn import metrics
+from sklearn import svm
+import scipy.linalg
+import sklearn.metrics
+import sklearn.neighbors
+
+
+def kernel(ker, X1, X2, gamma):
+    K = None
+    if not ker or ker == 'primal':
+        K = X1
+    elif ker == 'linear':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.linear_kernel(
+                np.asarray(X1).T, np.asarray(X2).T)
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    elif ker == 'rbf':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, np.asarray(X2).T, gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, None, gamma)
+    return K
+
+
+class TCA:
+    def __init__(self, kernel_type='primal', dim=30, lamb=1, gamma=1):
+        '''
+        Init func
+        :param kernel_type: kernel, values: 'primal' | 'linear' | 'rbf'
+        :param dim: dimension after transfer
+        :param lamb: lambda value in equation
+        :param gamma: kernel bandwidth for rbf kernel
+        '''
+        self.kernel_type = kernel_type
+        self.dim = dim
+        self.lamb = lamb
+        self.gamma = gamma
+
+    def fit(self, Xs, Xt):
+        '''
+        Transform Xs and Xt
+        :param Xs: ns * n_feature, source feature
+        :param Xt: nt * n_feature, target feature
+        :return: Xs_new and Xt_new after TCA
+        '''
+        X = np.hstack((Xs.T, Xt.T))
+        X /= np.linalg.norm(X, axis=0)
+        m, n = X.shape
+        ns, nt = len(Xs), len(Xt)
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        M = e * e.T
+        M = M / np.linalg.norm(M, 'fro')
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+        K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+        n_eye = m if self.kernel_type == 'primal' else n
+        a, b = np.linalg.multi_dot([K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
+        # a, b = K @ M @ K.T + self.lamb * np.eye(n_eye), K @ H @ K.T
+        w, V = scipy.linalg.eig(a, b)
+        ind = np.argsort(w)
+        A = V[:, ind[:self.dim]]
+        Z = A.T @ K
+        Z /= np.linalg.norm(Z, axis=0)
+
+        Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
+        return Xs_new, Xt_new
+
+    def fit_predict(self, Xs, Ys, Xt, Yt):
+        '''
+        Transform Xs and Xt, then make predictions on target using 1NN
+        :param Xs: ns * n_feature, source feature
+        :param Ys: ns * 1, source label
+        :param Xt: nt * n_feature, target feature
+        :param Yt: nt * 1, target label
+        :return: Accuracy and predicted_labels on the target domain
+        '''
+        Xs_new, Xt_new = self.fit(Xs, Xt)
+        clf = SVC(kernel='linear', random_state=0)
+        clf.fit(Xs_new, Ys.ravel())
+        y_pred = clf.predict(Xt_new)
+        acc = sklearn.metrics.accuracy_score(Yt, y_pred)
+
+        return acc, y_pred
+
+    # TCA code is done here. You can ignore fit_new and fit_predict_new.
+
+    def fit_new(self, Xs, Xt, Xt2):
+        '''
+        Map Xt2 to the latent space created from Xt and Xs
+        :param Xs : ns * n_feature, source feature
+        :param Xt : nt * n_feature, target feature
+        :param Xt2: n_s, n_feature, target feature to be mapped
+        :return: Xt2_new, mapped Xt2 with projection created by Xs and Xt
+        '''
+        # Computing projection matrix A from Xs an Xt
+        X = np.hstack((Xs.T, Xt.T))
+        X /= np.linalg.norm(X, axis=0)
+        m, n = X.shape
+        ns, nt = len(Xs), len(Xt)
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        M = e * e.T
+        M = M / np.linalg.norm(M, 'fro')
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+        K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+        n_eye = m if self.kernel_type == 'primal' else n
+        a, b = np.linalg.multi_dot(
+            [K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
+        w, V = scipy.linalg.eig(a, b)
+        ind = np.argsort(w)
+        A = V[:, ind[:self.dim]]
+
+        # Compute kernel with Xt2 as target and X as source
+        Xt2 = Xt2.T
+        K = kernel(self.kernel_type, X1=Xt2, X2=X, gamma=self.gamma)
+
+        # New target features
+        Xt2_new = A @ K
+        Xt2_new = Xt2_new.T
+        return Xt2_new
+
+    def fit_predict_new(self, Xt, Xs, Ys, Xt2, Yt2):
+        '''
+        Transfrom Xt and Xs, get Xs_new
+        Transform Xt2 with projection matrix created by Xs and Xt, get Xt2_new
+        Make predictions on Xt2_new using classifier trained on Xs_new
+        :param Xt: ns * n_feature, target feature
+        :param Xs: ns * n_feature, source feature
+        :param Ys: ns * 1, source label
+        :param Xt2: nt * n_feature, new target feature
+        :param Yt2: nt * 1, new target label
+        :return: Accuracy and predicted_labels on the target domain
+        '''
+        Xs_new, _ = self.fit(Xs, Xt)
+        Xt2_new = self.fit_new(Xs, Xt, Xt2)
+        clf = SVC(kernel='linear', random_state=0)
+        clf.fit(Xs_new, Ys.ravel())
+        y_pred = clf.predict(Xt2_new)
+        acc = sklearn.metrics.accuracy_score(Yt2, y_pred)
+        return acc, y_pred
+
+
+
+
+
+
+
+
+def kernel(ker, X1, X2, gamma):
+    K = None
+    if not ker or ker == 'primal':
+        K = X1
+    elif ker == 'linear':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.linear_kernel(
+                np.asarray(X1).T, np.asarray(X2).T)
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    elif ker == 'rbf':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, np.asarray(X2).T, gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, None, gamma)
+    return K
+
+
+def proxy_a_distance(source_X, target_X):
+    """
+    Compute the Proxy-A-Distance of a source/target representation
+    """
+    nb_source = np.shape(source_X)[0]
+    nb_target = np.shape(target_X)[0]
+
+    train_X = np.vstack((source_X, target_X))
+    train_Y = np.hstack((np.zeros(nb_source, dtype=int),
+                         np.ones(nb_target, dtype=int)))
+
+    clf = svm.LinearSVC(random_state=0)
+    clf.fit(train_X, train_Y)
+    y_pred = clf.predict(train_X)
+    error = metrics.mean_absolute_error(train_Y, y_pred)
+    dist = 2 * (1 - 2 * error)
+    return dist
+
+
+def estimate_mu(_X1, _Y1, _X2, _Y2):
+    adist_m = proxy_a_distance(_X1, _X2)
+    C = len(np.unique(_Y1))
+    epsilon = 1e-3
+    list_adist_c = []
+    for i in range(1, C + 1):
+        ind_i, ind_j = np.where(_Y1 == i), np.where(_Y2 == i)
+        Xsi = _X1[ind_i[0], :]
+        Xtj = _X2[ind_j[0], :]
+        adist_i = proxy_a_distance(Xsi, Xtj)
+        list_adist_c.append(adist_i)
+    adist_c = sum(list_adist_c) / C
+    mu = adist_c / (adist_c + adist_m)
+    if mu > 1:
+        mu = 1
+    if mu < epsilon:
+        mu = 0
+    return mu
+
+
+class BDA:
+    def __init__(self, kernel_type='primal', dim=30, lamb=1, mu=0.5, gamma=1, T=10, mode='BDA', estimate_mu=False):
+        '''
+        Init func
+        :param kernel_type: kernel, values: 'primal' | 'linear' | 'rbf'
+        :param dim: dimension after transfer
+        :param lamb: lambda value in equation
+        :param mu: mu. Default is -1, if not specificied, it calculates using A-distance
+        :param gamma: kernel bandwidth for rbf kernel
+        :param T: iteration number
+        :param mode: 'BDA' | 'WBDA'
+        :param estimate_mu: True | False, if you want to automatically estimate mu instead of manally set it
+        '''
+        self.kernel_type = kernel_type
+        self.dim = dim
+        self.lamb = lamb
+        self.mu = mu
+        self.gamma = gamma
+        self.T = T
+        self.mode = mode
+        self.estimate_mu = estimate_mu
+
+    def fit_predict(self, Xs, Ys, Xt, Yt):
+        '''
+        Transform and Predict using 1NN as JDA paper did
+        :param Xs: ns * n_feature, source feature
+        :param Ys: ns * 1, source label
+        :param Xt: nt * n_feature, target feature
+        :param Yt: nt * 1, target label
+        :return: acc, y_pred, list_acc
+        '''
+        list_acc = []
+        X = np.hstack((Xs.T, Xt.T))
+        X /= np.linalg.norm(X, axis=0)
+        m, n = X.shape
+        ns, nt = len(Xs), len(Xt)
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        C = len(np.unique(Ys))
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+        mu = self.mu
+        M = 0
+        Y_tar_pseudo = None
+        Xs_new = None
+        for t in range(self.T):
+            N = 0
+            M0 = e * e.T * C
+            if Y_tar_pseudo is not None and len(Y_tar_pseudo) == nt:
+                for c in range(0, C):
+                    e = np.zeros((n, 1))
+                    Ns = len(Ys[np.where(Ys == c)])
+                    Nt = len(Y_tar_pseudo[np.where(Y_tar_pseudo == c)])
+
+                    if self.mode == 'WBDA':
+                        Ps = Ns / len(Ys)
+                        Pt = Nt / len(Y_tar_pseudo)
+                        alpha = Pt / Ps
+                        mu = 1
+                    else:
+                        alpha = 1
+
+                    tt = Ys == c
+                    e[np.where(tt == True)] = 1 / Ns
+                    yy = Y_tar_pseudo == c
+                    ind = np.where(yy == True)
+                    inds = [item + ns for item in ind]
+                    e[tuple(inds)] = -alpha / Nt
+                    e[np.isinf(e)] = 0
+                    N = N + np.dot(e, e.T)
+
+            # In BDA, mu can be set or automatically estimated using A-distance
+            # In WBDA, we find that setting mu=1 is enough
+            if self.estimate_mu and self.mode == 'BDA':
+                if Xs_new is not None:
+                    mu = estimate_mu(Xs_new, Ys, Xt_new, Y_tar_pseudo)
+                else:
+                    mu = 0
+            M = (1 - mu) * M0 + mu * N
+            M /= np.linalg.norm(M, 'fro')
+            K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+            n_eye = m if self.kernel_type == 'primal' else n
+            a, b = K @ M @ K.T + self.lamb * np.eye(n_eye), K @ H @ K.T
+            w, V = scipy.linalg.eig(a, b)
+            ind = np.argsort(w)
+            A = V[:, ind[:self.dim]]
+            Z = A.T @ K
+            Z /= np.linalg.norm(Z, axis=0)
+            Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
+
+            clf = SVC(kernel='linear', random_state=0)
+            clf.fit(Xs_new, Ys.ravel())
+            Y_tar_pseudo = clf.predict(Xt_new)
+            acc = sklearn.metrics.accuracy_score(Yt, Y_tar_pseudo)
+            list_acc.append(acc)
+            print('{} iteration [{}/{}]: Acc: {:.4f}'.format(self.mode, t + 1, self.T, acc))
+        return acc, Y_tar_pseudo, list_acc,Xs_new, Xt_new
+
+
+
+
+
+
+def kernel_1(ker, X1, X2, gamma):
+    K = None
+    if not ker or ker == 'primal':
+        K = X1
+    elif ker == 'linear':
+        if X2:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T, np.asarray(X2).T)
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    elif ker == 'rbf':
+        if X2:
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, np.asarray(X2).T, gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, None, gamma)
+    return K
+
+
+class JDA:
+    def __init__(self, kernel_type='primal', dim=30, lamb=1, gamma=1, T=10):
+        '''
+        Init func
+        :param kernel_type: kernel, values: 'primal' | 'linear' | 'rbf'
+        :param dim: dimension after transfer
+        :param lamb: lambda value in equation
+        :param gamma: kernel bandwidth for rbf kernel
+        :param T: iteration number
+        '''
+        self.kernel_type = kernel_type
+        self.dim = dim
+        self.lamb = lamb
+        self.gamma = gamma
+        self.T = T
+
+    def fit_predict(self, Xs, Ys, Xt, Yt):
+        '''
+        Transform and Predict using 1NN as JDA paper did
+        :param Xs: ns * n_feature, source feature
+        :param Ys: ns * 1, source label
+        :param Xt: nt * n_feature, target feature
+        :param Yt: nt * 1, target label
+        :return: acc, y_pred, list_acc
+        '''
+        list_acc = []
+        X = np.hstack((Xs.T, Xt.T))
+        X /= np.linalg.norm(X, axis=0)
+        m, n = X.shape
+        ns, nt = len(Xs), len(Xt)
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        C = len(np.unique(Ys))
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+
+        M = 0
+        Y_tar_pseudo = None
+        for t in range(self.T):
+            N = 0
+            M0 = e * e.T * C
+            if Y_tar_pseudo is not None and len(Y_tar_pseudo) == nt:
+                for c in range(0, C):
+                    e = np.zeros((n, 1))
+                    tt = Ys == c
+                    e[np.where(tt == True)] = 1 / len(Ys[np.where(Ys == c)])
+                    yy = Y_tar_pseudo == c
+                    ind = np.where(yy == True)
+                    inds = [item + ns for item in ind]
+                    e[tuple(inds)] = -1 / (len(Y_tar_pseudo[np.where(Y_tar_pseudo == c)])+1)
+                    e[np.isinf(e)] = 0
+                    N = N + np.dot(e, e.T)
+            M = M0 + N
+            M = M / np.linalg.norm(M, 'fro')
+            K = kernel_1(self.kernel_type, X, None, gamma=self.gamma)
+            n_eye = m if self.kernel_type == 'primal' else n
+            a, b = np.linalg.multi_dot([K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
+            w, V = scipy.linalg.eig(a, b)
+            ind = np.argsort(w)
+            A = V[:, ind[:self.dim]]
+            Z = np.dot(A.T, K)
+            Z /= np.linalg.norm(Z, axis=0)
+            Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
+
+            clf = SVC(kernel='linear', random_state=0)
+            clf.fit(Xs_new, Ys.ravel())
+            Y_tar_pseudo = clf.predict(Xt_new)
+            acc = sklearn.metrics.accuracy_score(Yt, Y_tar_pseudo)
+            list_acc.append(acc)
+            print('JDA iteration [{}/{}]: Acc: {:.4f}'.format(t + 1, self.T, acc))
+        return acc, Y_tar_pseudo, list_acc
+
+
+
